890324665d9c7165b0265b70e0130075c552a903
Victor Dominguez <vd071090@gmail.com>
2020-04-09 23:33:33 -0400

    added more info in forcast cards

--- my-app/src/components/Card/weatherCard.js
+++ my-app/src/components/Card/weatherCard.js
@@ -4,15 +4,24 @@ import Container from "react-bootstrap/Container";
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
 import { CardDeck } from "react-bootstrap";
+import "./style.css";
 
 const WeatherCard = ({ list }) => (
   <Container>
     <Row>
       <Col>
         <CardDeck>
-          <Card>
+          <Card className="forcast">
+            <Card.Header>
+              <h3>{list.dt}</h3>
+            </Card.Header>
             <Card.Body>
-              <h2>{list.weather[0].description}</h2>
+              <h5>
+                Sunrise: {list.sunrise} Sunset: {list.sunset}
+              </h5>
+            </Card.Body>
+            <Card.Body>
+              <h4>{list.weather[0].description}</h4>
             </Card.Body>
           </Card>
         </CardDeck>
